3b08a6fe90eb8a2f252fd1b4eebd477bd87f2986
al <cmdoptesc@users.noreply.github.com>
2024-09-25 08:23:47 -0700

    Personalize devenv settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,7 +23,99 @@ export default tseslint.config(
     },
 
     rules: {
-      '@typescript-eslint/explicit-function-return-type': 'warn',
+      'no-param-reassign': 'error',
+
+      // Typescript can infer these just fine
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+      // @typescript-eslint/recommended-requiring-type-checking`
+      '@typescript-eslint/no-loss-of-precision': 'warn',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unsafe-assignment': 'warn',
+      '@typescript-eslint/no-unsafe-call': 'warn',
+      '@typescript-eslint/no-unsafe-member-access': 'warn',
+      '@typescript-eslint/no-unsafe-return': 'warn',
+      '@typescript-eslint/restrict-plus-operands': 'warn',
+      '@typescript-eslint/restrict-template-expressions': 'warn',
+      '@typescript-eslint/unbound-method': 'warn',
+
+      // Sometimes it's nice to use `async` for functions without `await` to make the function async.
+      '@typescript-eslint/require-await': 'off',
+
+      // Sometimes it's nice to call an async function in a fire+forget way.
+      '@typescript-eslint/no-floating-promises': 'off',
+
+      // Explicit any is okay
+      '@typescript-eslint/no-explicit-any': 'off',
+
+      // Allow unused args and vars when marked with an underscore
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+
+      // This seems to have issues with async functions inside of setTimeout.
+      // Disabling `checksVoidReturn` seems to fix this.
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        { checksVoidReturn: false },
+      ],
+
+      // Enforce standard naming conventions for typescript variables.
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'default',
+          format: ['camelCase'],
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: 'variable',
+          format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: 'typeLike',
+          format: ['PascalCase'],
+        },
+        {
+          selector: 'enumMember',
+          format: ['PascalCase'],
+        },
+        {
+          selector: 'property',
+          format: null,
+        },
+        // PascalCase for React components
+        {
+          selector: 'class',
+          format: ['PascalCase'],
+        },
+        {
+          selector: 'function',
+          format: ['camelCase', 'PascalCase'],
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: 'default',
+          format: ['camelCase', 'PascalCase'],
+          filter: {
+            regex: 'Component$',
+            match: true,
+          },
+        },
+        {
+          selector: 'variable',
+          types: ['function'],
+          format: ['camelCase', 'PascalCase'],
+          leadingUnderscore: 'allow',
+        },
+      ],
     },
 
     languageOptions: {
